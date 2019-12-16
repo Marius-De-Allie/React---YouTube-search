9c73de44b78f5cbe9b3d55ed9abe60bd8885fe8c
Marius De Allie <spectre4300s@gmail.com>
2019-12-16 14:57:41 -0500

    import App from /src/components/App into src/index file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 // Import dependancies.
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
 // Render App component.
 ReactDOM.render(<App />, document.getElementById('root'));
