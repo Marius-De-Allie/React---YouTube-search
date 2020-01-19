ebcc71e1bc35ffc8b32643115e3692ae515c6648
Marius De Allie <spectre4300s@gmail.com>
2020-01-19 01:26:58 -0500

    Add state object to SearchBar component setting value to searchTerm: ''

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = {
+    searchTerm: ''
+  };
   render () {
     return (
       <div className="search-bar ui segment">
