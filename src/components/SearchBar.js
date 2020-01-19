19e066ead455f111f577ed0f402ec6c773635da0
Marius De Allie <spectre4300s@gmail.com>
2020-01-19 01:08:12 -0500

    Add className attribute with value of search-bar ui segment to SearchBar component root div JSX element

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,9 @@ import React from 'react';
 class SearchBar extends React.Component {
   render () {
     return (
-      <div>SearchBar</div>
+      <div className="search-bar ui segment">
+
+      </div>
     );
   }
 }
