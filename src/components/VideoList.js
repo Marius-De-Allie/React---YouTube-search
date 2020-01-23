e307e7c58f1dde21083f31b492be33be94786c1e
Marius De Allie <spectre4300s@gmail.com>
2020-01-23 17:43:07 -0500

    Map over this.state.videos array using map method in VideoList component, assign returned array of list items to renderedList variable

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,6 +3,9 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideoList = ({videos}) => {
+  const renderedList = videos.map((video) => {
+    return <VideoItem />
+  });
   return (
     <div>
       <VideoItem />
