324f75ca27875f009b3f9c810eec262e36a70002
Marius De Allie <spectre4300s@gmail.com>
2020-01-26 22:31:52 -0500

    Add new div element with className of ui embed to VideoDetail component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -6,6 +6,8 @@ const VideoDetail = ({video}) => {
   }
   return (
     <div>
+      <div className="ui embed">
+      </div>
       <div className="ui segment">
         <h4 className="ui header">
           {video.snippet.title}
