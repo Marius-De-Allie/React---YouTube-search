45e06eed4680a7b7b1467a2989ad82f240d3608d
Marius De Allie <spectre4300s@gmail.com>
2020-01-26 22:44:13 -0500

    Add iframe element as child element of div with className of ui embed within VideoDetail component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -7,6 +7,7 @@ const VideoDetail = ({video}) => {
   return (
     <div>
       <div className="ui embed">
+        <iframe />
       </div>
       <div className="ui segment">
         <h4 className="ui header">
