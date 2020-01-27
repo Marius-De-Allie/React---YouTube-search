e890d8f41e3df94452df1371b94684f5d45c6aec
Marius De Allie <spectre4300s@gmail.com>
2020-01-26 23:32:48 -0500

    Add div element with className of five wide column as parent to VideoList element within App component

--- src/components/App.js
+++ src/components/App.js
@@ -45,7 +45,9 @@ onVideoSelect = (video) => {
             <div className="eleven wide column">
               <VideoDetail video={this.state.selectedVideo} />
             </div>
-            <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+            <div className="five wide column">
+              <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+            </div>
           </div>
         </div>
       </div>
